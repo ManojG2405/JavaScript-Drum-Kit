b11487f6e5acc02000c53281c1d6186bad58f71c
ManojG2405 <Gawade.manoj77@gmail.com>
2022-05-19 17:09:03 +0530

    Change attribute through Js

--- script.js
+++ script.js
@@ -23,7 +23,6 @@
 //   elm[i].innerHTML = "Hello";
 // }
 
-
 //Query Selector
 
 // let elm = document.querySelector("p.intro");
@@ -39,11 +38,10 @@
 //   elm[i].innerHTML = "Hello";
 // }
 
-
 // ****** Traversing elements in JavaScript *****
 // let elm = document.getElementById("intro");
 // let prt = elm.parentElement;
-// // same as we can use firstChildElement, lastChildElement & children 
+// // same as we can use firstChildElement, lastChildElement & children
 // prt.innerHTML="Hello re Baba";
 // console.log(prt);
 
@@ -53,24 +51,56 @@
 // // Same as we can use nextElementSibling
 // console.log(sib);
 
-
-//  *********** innerHTML Demo ************ 
+//  *********** innerHTML Demo ************
 // let elm = document.getElementById("intro").innerHTML = "<p>Content changed using JavaScript</p>";
 
 // Append Element in HTML
 
-let elm = document.getElementById("intro") // Parent
+// let elm = document.getElementById("intro") // Parent
 
-let h1 = document.createElement('h1');
+// let h1 = document.createElement('h1');
 
-h1.className = "try bye";
-h1.id = "introId";
-// let text = document.createTextNode("This is h1 tag");
-// h1.appendChild(text)
-h1.textContent = "Added Content"
+// h1.className = "try bye";
+// h1.id = "introId";
+// // let text = document.createTextNode("This is h1 tag");
+// // h1.appendChild(text)
+// h1.textContent = "Added Content"
 
 // elm.appendChild(h1);
 // document.body.appendChild(h1);
 
+// ****** insertBefore / after ***********
+//  use inertBefore
 
-// ****** insertBefore ***********
+// ########## Remove Child element ########
+
+// let parent = document.getElementById("menu");
+// let elm = parent.firstElementChild;
+// parent.removeChild(elm);
+// document.body.removeChild(menu);
+
+// ************** Clone or copy element ************
+
+// let menu = document.getElementById("menu");
+// let cloneElm = menu.cloneNode(true); // use false to copy only parent element
+// // console.log(cloneElm);
+// cloneElm.id = "menu2"
+// document.body.appendChild(cloneElm);
+
+// ************** Replace element ***********
+
+// let menu = document.getElementById("menu");
+// let newItem = document.createElement("li");
+// newItem.textContent = "List E";
+// let replace = menu.firstElementChild.nextElementSibling;
+// menu.replaceChild(newItem, replace);
+
+// **************** Insert Adjacent HTML ************
+// let adj = document.getElementById("power");
+// let newELm = "<h1> New h1</h1>";
+// adj.insertAdjacentHTML('afterend', newELm)
+// Properties are beforebegin, afterbegin, beforeend, afterend.
+
+// *********** Change attribute through Js ********
+let btn = document.getElementById("btn");
+btn.setAttribute("name", "form1");
